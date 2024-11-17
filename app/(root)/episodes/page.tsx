6fe272aaa1cbfe768a8f46d61d00d88e76e6525e
wade-loccisano <wade.loccisano@gmail.com>
2024-11-17 17:44:16 -0500

    Episode list

--- app/(root)/episodes/page.tsx
+++ app/(root)/episodes/page.tsx
@@ -4,10 +4,30 @@ export default async function Episodes({ searchParams }: {
     console.log(searchParams)
     return (
         <>
+            <section className="section_container">
+                <h1 className="md:text-4xl font-bold">
+                    Episodes
+                </h1>
+                <div className="py-4">
+                    <input
+                        className='my-2 p-2 w-full border rounded'
+                        placeholder='Search'
+                    >
+                    </input>
+                </div>
+                <div>
+                    {/* nextjs paginated list  */}
+                    <div className="bg-[#fefe21] p-4">
+                        <div className="text-sm text-slate-400">
+                            Nov 19, 2024
+                        </div>
+                        <div className="font-bold text-lg">
+                            Final Fantasy 7, Crisis Core, and the Confusing Remake
+                        </div>
+                    </div>
+                </div>
+            </section>
             {/* <div className="max-w-6xl mx-auto pt-8"> */}
-            <div className="mx-auto pt-8">
-                Episode List Goes Here
-            </div>
         </>
     );
 }
